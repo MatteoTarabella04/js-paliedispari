246e1974bf4caa6d49409183d3ee7f9a13607512
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-16 18:10:47 +0100

    add check for number choise

--- pari-dispari/assets/js/main.js
+++ pari-dispari/assets/js/main.js
@@ -14,12 +14,18 @@ if (userChoise != 'pari' && userChoise != 'dispari') {
 
    // istruzione che ricarica la pagina se si inserisce una scelta sbagliata
    window.location.reload(true);
-
 } else {
+
    // ora chiedo di inserire un numero da 1 a 5
    const userNumber = Number(prompt('Ora scegli un numero tra 1 e 5'));
    console.log(userNumber);
+
+   if (isNaN(userNumber)) {
+      alert('Valore inserito non numerico')
+      window.location.reload(true);
+   }
 }
+
 
 
 // *****FUNZIONI*****
