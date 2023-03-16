79a5ce93b17fd77e05dc240859cbe6638b4f2b36
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-16 16:54:18 +0100

    add function reverse to reverse the user input

--- palidroma/assets/js/main.js
+++ palidroma/assets/js/main.js
@@ -7,3 +7,18 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 const word = prompt('Inserire la parola');
 console.log(word);
 
+// creo una funzione che mi permette di leggere al contrario la parola inserita 
+function reverse(userWord){
+   /* 
+   1- utilizzo split per restituire e dividere la stringa inserita in un array di sottostringhe
+
+   2- utilizzo reverse per invertire l'ordine dell'array
+
+   3 infine join per prelevare gli elementi dall'array e convertirli di nuovo in stringa 
+   */
+
+  // return mi restituirà un valore booleano true/false
+   return userWord == userWord.split('').reverse().join('');
+}
+console.log(reverse(word));
+
