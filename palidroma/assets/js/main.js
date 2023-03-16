138e3b74de42ebcb655e95c157f0ac30ac9a8c78
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-16 16:31:29 +0100

    add prompt for input

--- palidroma/assets/js/main.js
+++ palidroma/assets/js/main.js
@@ -3,3 +3,7 @@ Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// chiedo all'utenete di inserire la parla da verificare
+const word = prompt('Inserire la parola');
+console.log(word);
+
